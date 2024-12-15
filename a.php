5e70ae19b51d5b395a94ddce5505708ec1bc6085
thia <rahmathia@gmail.com>
2024-12-14 22:25:18 -0800

    menambahkan file a.pphp

---
+++ a.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut perspiciatis temporibus qui aliquid voluptatibus quam asperiores repudiandae exercitationem unde placeat veniam odit iure sunt quae, fuga similique tempora doloribus reiciendis dignissimos assumenda repellendus a rerum? Magni facilis vero repellendus praesentium, aut perferendis accusamus ut consequuntur explicabo. Facere quae sunt at delectus, cupiditate magni harum. Temporibus quam eveniet odio perspiciatis dolores. Tempora consequuntur sint eligendi quaerat odio inventore et possimus, nesciunt enim voluptatum magnam mollitia tenetur quo hic omnis. Repudiandae dolore libero quisquam molestias. Accusamus a natus omnis debitis vero voluptas magni, corporis suscipit dolorum odio explicabo dignissimos. Quae, magnam ut.</p>
+</body>
+</html>
---
+++ belajarjs.js
@@ -0,0 +1,3 @@
+console.log("lorem");
+console.log("ipsum");
+console.log("dolor");
